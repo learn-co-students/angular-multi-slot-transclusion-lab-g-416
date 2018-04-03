c977b4d667d4d2518c84aaa30f95af1972718d1c
r0tiart <antley.chu@gmail.com>
2018-04-02 20:05:54 -0400

    Done.

--- js/app/directives/UserProfile.js
+++ js/app/directives/UserProfile.js
@@ -1,10 +1,16 @@
 function UserProfile() {
 	return {
-		transclude: true,
+		transclude: {
+			name: 'h4',
+			position: 'h6',
+			description: 'p'
+		},
 		template: [
 			'<div>',
 				'<h3>User Profile</h3>',
-				'<div ng-transclude></div>',
+				'<div>Name: <h4 ng-transclude="name"></h4></div>',
+				'<div>Position: <h6 ng-transclude="position"></h6></div>',
+        		'<div>Description: <p ng-transclude="description"></p></div>',
 			'</div>'
 		].join('')
 	}
